a906e3ef9fec466fae4eac538ae5e48504ca01b8
anmolchawla <anmolcha@usc.edu>
2018-09-05 09:36:02 -0700

    First One

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # INF 554 Assignment 2
 
 Update this README as needed.
+
+This assignment is based on the same data of the previous assignment.
+For this assignment only one year from the countries-population-time data was selected.
+
+As China, India and United states operate on a different magnitude, the bubble chart to show the countries based on text and colour, with their radius corresponding to the "Normalized"
+ value (1 - 100)  was selcted. As previously mentioned since there scale was different the text had to be made transparent in order to show the viewer the differnce in the magnitudes.
+
+For this assignment, a seperate Javascript and CSS file were created in addition to the index.html.
+
+The first step was to install and run node.js so that a local server can be created on the system that could allow a continous sync while editing, hence saying valuable time.
+
+As I was previously involved in web development for another class, I had Node Js pre - installed as shown below.
+
+(-- add image-- )
+
+After that NPM was installed to allow for browser sync, as shown below.
+
+(-- add image--)
+
+Git was cloned to a local desktop and a commit was made to ensure everything was on the Clasroom page.
+
+
+
+
---
+++ chart.js
@@ -0,0 +1,230 @@
+
+
+var svg = document.getElementById("check");
+svg.setAttribute("viewBox", "0 0 210 297"); 
+svg.setAttribute("width","210mm");
+svg.setAttribute("height","297mm");
+
+
+
+var rectangle = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+rectangle.setAttribute("opacity","0.23");
+rectangle.setAttribute("fill", "#eeffaa");
+rectangle.setAttribute("fill-opacity","1");
+rectangle.setAttribute("stroke","#000000");
+rectangle.setAttribute("stroke-width","0.55199999");
+rectangle.setAttribute("stroke-miterlimit","4");
+rectangle.setAttribute("stroke-opacity","1");
+rectangle.setAttribute("width", "206.92007");
+rectangle.setAttribute("height", "294.85172");
+svg.appendChild(rectangle); //append to svg
+
+
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("fill", "#ffd5d5");
+circle.setAttribute("stroke", "#000000");
+circle.setAttribute("stroke-width", "0.7586056");
+circle.setAttribute("stroke-miterlimit", "4");
+circle.setAttribute("cx", 152.67378);
+circle.setAttribute("cy", 62.698563);
+circle.setAttribute("r", 34.183701);
+svg.appendChild(circle); //append to svg
+
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("fill", "#ffccaa");
+circle.setAttribute("stroke", "#000000");
+circle.setAttribute("stroke-width", "0.06887064");
+circle.setAttribute("stroke-miterlimit", "4");
+circle.setAttribute("cx", "123.3664");
+circle.setAttribute("cy", "19.221638");
+circle.setAttribute("r","3.1033959");
+svg.appendChild(circle); //append to svg
+
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("fill", "#c8ab37");
+circle.setAttribute("stroke", "#000000");
+circle.setAttribute("stroke-width", "0.05020271");
+circle.setAttribute("stroke-miterlimit", "4");
+circle.setAttribute("cx", 157.90044);
+circle.setAttribute("cy", 137.98083);
+circle.setAttribute("r", 2.2621961);
+svg.appendChild(circle); //append to svg
+
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("fill", "#668000");
+circle.setAttribute("stroke", "#000000");
+circle.setAttribute("stroke-width", "0.01766633");
+circle.setAttribute("stroke-miterlimit", "4");
+circle.setAttribute("cx", "93.786903");
+circle.setAttribute("cy", "155.14745");
+circle.setAttribute("r", "0.79606646");
+svg.appendChild(circle); //append to svg
+
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("fill", "#aa0000");
+circle.setAttribute("stroke", "#000000");
+circle.setAttribute("stroke-width", "0.01546132");
+circle.setAttribute("stroke-miterlimit", "4");
+circle.setAttribute("cx", 25.046724);
+circle.setAttribute("cy", 183.09659);
+circle.setAttribute("r", 0.69670594);
+svg.appendChild(circle); //append to svg
+
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("fill", "#ff0000");
+circle.setAttribute("stroke", "#000000");
+circle.setAttribute("stroke-width", "0.07176485");
+circle.setAttribute("stroke-miterlimit", "4");
+circle.setAttribute("cx", 117.77166);
+circle.setAttribute("cy", 213.92047);
+circle.setAttribute("r", 3.2338123);
+svg.appendChild(circle); //append to svg
+
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("fill", "#ffcc00");
+circle.setAttribute("stroke", "#000000");
+circle.setAttribute("stroke-width", "0.262501");
+circle.setAttribute("stroke-miterlimit", "4");
+circle.setAttribute("cx", "80.734192");
+circle.setAttribute("cy", "256.90289");
+circle.setAttribute("r", "11.828618");
+svg.appendChild(circle); //append to svg
+
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("fill", "#fff6d5");
+circle.setAttribute("stroke", "#000000");
+circle.setAttribute("stroke-width", "1.09742427");
+circle.setAttribute("stroke-miterlimit", "4");
+circle.setAttribute("cx", "51.308529");
+circle.setAttribute("cy", "51.789677");
+circle.setAttribute("r", "49.451286");
+svg.appendChild(circle); //append to svg
+
+
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("fill", "#d4550");
+circle.setAttribute("stroke", "#000000");
+circle.setAttribute("stroke-width", "0.01097424");
+circle.setAttribute("stroke-miterlimit", "4");
+circle.setAttribute("cx", "30.221329");
+circle.setAttribute("cy", "119.12675");
+circle.setAttribute("r", "0.49451289");
+svg.appendChild(circle); //append to svg
+
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("font-family", "sans-serif");
+text.setAttribute("font-size","10.58333302");
+text.setAttribute("opacity","0.23000004");
+text.setAttribute("x","139.26375");
+text.setAttribute("y", "66.638893");
+text.innerHTML = "India";
+svg.appendChild(text); //append to svg
+
+
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("font-family", "sans-serif");
+text.setAttribute("font-size","10.58333302");
+text.setAttribute("opacity","0.23000004");
+text.setAttribute("x","105.45535");
+text.setAttribute("y", "22.988848");
+text.innerHTML = "France";
+svg.appendChild(text); //append to svg
+
+
+
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("font-family", "sans-serif");
+text.setAttribute("font-size","10.58333302");
+text.setAttribute("opacity","0.23000004");
+text.setAttribute("x","83.15476");
+text.setAttribute("y", "104.60632");
+text.innerHTML = "Portugal";
+svg.appendChild(text); //append to svg
+
+
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("font-family", "sans-serif");
+text.setAttribute("font-size","10.58333302");
+text.setAttribute("opacity","0.23000004");
+text.setAttribute("x","10.10278");
+text.setAttribute("y", "122.63337");
+text.innerHTML = "Singapore";
+svg.appendChild(text); //append to svg
+
+
+
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("font-family", "sans-serif");
+text.setAttribute("font-size","10.58333302");
+text.setAttribute("opacity","0.23000004");
+text.setAttribute("x","143.30992");
+text.setAttribute("y", "141.7164");
+text.innerHTML = "Spain";
+svg.appendChild(text); //append to svg
+
+
+
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("font-family", "sans-serif");
+text.setAttribute("font-size","10.58333302");
+text.setAttribute("opacity","0.23000004");
+text.setAttribute("x","74.461288");
+text.setAttribute("y", "159.30275");
+text.innerHTML = "Sweden";
+svg.appendChild(text); //append to svg
+
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("font-family", "sans-serif");
+text.setAttribute("font-size","10.58333302");
+text.setAttribute("opacity","0.23000004");
+text.setAttribute("x","1.8708872");
+text.setAttribute("y", "186.61768");
+text.innerHTML = "Switzerland";
+svg.appendChild(text); //append to svg
+
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("font-family", "sans-serif");
+text.setAttribute("font-size","10.58333302");
+text.setAttribute("opacity","0.23000004");
+text.setAttribute("x","81.944832");
+text.setAttribute("y", "216.17769");
+text.innerHTML = "United Kingdom";
+svg.appendChild(text); //append to svg
+
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("fill", "#ffcc00");
+circle.setAttribute("stroke", "#000000");
+circle.setAttribute("stroke-width", "0.01916368");
+circle.setAttribute("stroke-miterlimit", "4");
+circle.setAttribute("cx", "103.47926");
+circle.setAttribute("cy", "101.29455");
+circle.setAttribute("r","0.86353916");
+svg.appendChild(circle); //append to svg
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("font-family", "sans-serif");
+text.setAttribute("font-size","10.58333302");
+text.setAttribute("opacity","0.23000004");
+text.setAttribute("x","44.5271");
+text.setAttribute("y", "260.33063");
+text.innerHTML = "United States";
+svg.appendChild(text); //append to svg
+
+
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("font-family", "sans-serif");
+text.setAttribute("font-size","10.58333302");
+text.setAttribute("opacity","0.23000004");
+text.setAttribute("x","35.170776");
+text.setAttribute("y", "54.709663");
+text.innerHTML = "China";
+svg.appendChild(text); //append to svg
